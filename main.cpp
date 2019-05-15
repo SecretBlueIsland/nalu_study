011b6f2dd39382d8c957fe2ff7e60e723b5a7f9a
chenchen <chenchen@LAPTOP-P1EK1C88>
2019-05-15 22:55:08 +0800

    初步精简代码

--- main.cpp
+++ main.cpp
@@ -7,12 +7,11 @@ extern "C"
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	AVOutputFormat* ofmt = NULL;
 	//£¨Input AVFormatContext and Output AVFormatContext£©
-	AVFormatContext* ifmt_ctx = NULL, *ofmt_ctx = NULL;
+	AVFormatContext* ifmt_ctx = NULL;
 	AVPacket pkt;
 	int ret, i;
-	int videoindex = -1, audioindex = -1;
+	int videoindex = 1;
 	int frame_index = 0;
 	
 	uint8_t sps[100];
@@ -42,52 +41,7 @@ int _tmain(int argc, _TCHAR* argv[])
 		return ret;
 	}
 
-
-
-
-	//Output
-	avformat_alloc_output_context2(&ofmt_ctx, NULL, NULL, origin_out_filename);
-	if (!ofmt_ctx) {
-		printf("Could not create output context\n");
-		ret = AVERROR_UNKNOWN;
-	}
-	ofmt = ofmt_ctx->oformat;	
-
 //	av_dump_format(ifmt_ctx, 0, in_filename, 0);
-
-	for (i = 0; i < ifmt_ctx->nb_streams; i++) {
-		//Create output AVStream according to input AVStream
-		AVStream* in_stream = ifmt_ctx->streams[i];
-		AVStream* out_stream = NULL;
-
-		if (ifmt_ctx->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO) {
-			videoindex = i;
-			out_stream = avformat_new_stream(ofmt_ctx, in_stream->codec->codec);
-		}
-		else if (ifmt_ctx->streams[i]->codec->codec_type == AVMEDIA_TYPE_AUDIO) {
-			audioindex = i;
-			continue;
-		}
-	
-		if (!out_stream) {
-		printf("Failed allocating output stream\n");
-		ret = AVERROR_UNKNOWN;
-		}
-		//Copy the settings of AVCodecContext
-		if (avcodec_copy_context(out_stream->codec, in_stream->codec) < 0) {
-			printf("Failed to copy context from input to output stream codec context\n");
-	}
-	out_stream->codec->codec_tag = 0;
-	if (ofmt_ctx->oformat->flags & AVFMT_GLOBALHEADER)
-		out_stream->codec->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
-
-	}
-
-	if (!(ofmt->flags & AVFMT_NOFILE)) {
-		if (avio_open(&ofmt_ctx->pb, origin_out_filename, AVIO_FLAG_WRITE) < 0) {
-			printf("Could not open output file '%s'", origin_out_filename);
-		}
-	}
 
 	for (int i = 0; i < ifmt_ctx->streams[videoindex]->codec->extradata_size; i++)
 	{
@@ -107,13 +61,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	{
 		pps[i] = ifmt_ctx->streams[videoindex]->codec->extradata[i + 8 + 2 + 1 + spsLength];
 	}
-
-	//Write file header
-	if (avformat_write_header(ofmt_ctx, NULL) < 0) {
-		printf("Error occurred when opening output file\n");
-	}
 	
-
 	int flag = 1;
 
 	av_init_packet(&pkt);
@@ -126,61 +74,40 @@ int _tmain(int argc, _TCHAR* argv[])
 
 
 	while (1) {
-		AVStream* in_stream, * out_stream;
 		//Get an AVPacket
 		if (av_read_frame(ifmt_ctx, &pkt) < 0)
 			break;
-		in_stream = ifmt_ctx->streams[pkt.stream_index];
-//		out_stream = ofmt_ctx->streams[pkt.stream_index];	
 		if (pkt.stream_index == videoindex) {
-			out_stream = ofmt_ctx->streams[0];
-			printf("Write Video Packet. size:%d\tpts:%lld\n", pkt.size, pkt.pts);	
+//			printf("Write Video Packet. size:%d\tpts:%lld\n", pkt.size, pkt.pts);	
 		}
 		else {
 			continue;
 		}
 		
-		AVPacket tmppkt;
-		if (in_stream->codec->codec_type == AVMEDIA_TYPE_VIDEO)
+		if (flag)
 		{
+			fwrite(startcode, 4, 1, fp);
+			fwrite(sps, spsLength, 1, fp);
+			fwrite(startcode, 4, 1, fp);
+			fwrite(pps, ppsLength, 1, fp);
 
-			if (flag)
-			{
-				fwrite(startcode, 4, 1, fp);
-				fwrite(sps, spsLength, 1, fp);
-				fwrite(startcode, 4, 1, fp);
-				fwrite(pps, ppsLength, 1, fp);
+			pkt.data[0] = 0x00;
+			pkt.data[1] = 0x00;
+			pkt.data[2] = 0x00;
+			pkt.data[3] = 0x01;
+			fwrite(pkt.data, pkt.size, 1, fp);
 
-				pkt.data[0] = 0x00;
-				pkt.data[1] = 0x00;
-				pkt.data[2] = 0x00;
-				pkt.data[3] = 0x01;
-				fwrite(pkt.data, pkt.size, 1, fp);
-
-				flag = 0;
-			}
-			else
-			{
-				pkt.data[0] = 0x00;
-				pkt.data[1] = 0x00;
-				pkt.data[2] = 0x00;
-				pkt.data[3] = 0x01;
-				fwrite(pkt.data, pkt.size, 1, fp);
-			}
+			flag = 0;
 		}
-			pkt.pts = av_rescale_q_rnd(pkt.pts, in_stream->time_base, out_stream->time_base, (AVRounding)(AV_ROUND_NEAR_INF | AV_ROUND_PASS_MINMAX));
-			pkt.dts = av_rescale_q_rnd(pkt.dts, in_stream->time_base, out_stream->time_base, (AVRounding)(AV_ROUND_NEAR_INF | AV_ROUND_PASS_MINMAX));
-			pkt.duration = av_rescale_q(pkt.duration, in_stream->time_base, out_stream->time_base);
-			pkt.pos = -1;
+		else
+		{
+			pkt.data[0] = 0x00;
+			pkt.data[1] = 0x00;
+			pkt.data[2] = 0x00;
+			pkt.data[3] = 0x01;
+			fwrite(pkt.data, pkt.size, 1, fp);
+		}
 
-			pkt.stream_index = 0;
-		//Write
-			if (av_interleaved_write_frame(ofmt_ctx, &pkt) < 0) {
-				printf("Error muxing packet\n");
-				break;
-			}
-		//}
-		//printf("Write %8d frames to output file\n", frame_index);
 		av_free_packet(&pkt);
 		frame_index++;
 	}
@@ -188,6 +115,5 @@ int _tmain(int argc, _TCHAR* argv[])
 	fclose(fp);
 	fp = NULL;
 
-	av_write_trailer(ofmt_ctx);
 	return 0;
 }
